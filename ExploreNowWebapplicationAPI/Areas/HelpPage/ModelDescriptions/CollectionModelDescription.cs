851ec2fa627376a99131828a31e8af2bd37f2c3e
Jonathan Kennås <jk95@live.se>
2020-09-02 11:52:59 +0200

    Skapande av projekt

---
+++ ExploreNowWebapplicationAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ExploreNowWebapplicationAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ExploreNowWebapplicationAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExploreNowWebapplicationAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
