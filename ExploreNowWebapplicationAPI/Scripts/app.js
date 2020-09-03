c55656d0c975631155f8c623d0ef3124079b6180
Jonathan Kennås <jk95@live.se>
2020-09-03 12:09:36 +0200

    Lagt till markörer för Google Maps, endast hårdkodat för test

--- ExploreNowWebapplicationAPI/Scripts/app.js
+++ ExploreNowWebapplicationAPI/Scripts/app.js
@@ -65,102 +65,56 @@
     }
 
     // Google maps
-    loadMapsJSAPI();
-
-    function runApp() {
-        console.log('Maps JS API loaded');
-        const map = displayMap();
-        const markers = addMarkers(map);
-        clusterMarkers(map, markers);
-        addPanToMarker(map, markers);
-    }
-
-    function loadMapsJSAPI() {
-        const googleMapsAPIKey = 'AIzaSyClQVtfHOJaUG2YXyLbtbJ73Jm__81vaTU';
-        const googleMapsAPIURI = `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPIKey}&callback=runApp`;
-        const script = document.createElement('script');
-
-        script.src = googleMapsAPIURI;
-        script.defer = true;
-        script.async = true;
-
-        window.runApp = runApp;
-        document.head.appendChild(script);
-    }
-
-    function displayMap() {
-        const mapOptions = {
-            center: { lat: -33.860664, lng: 151.208138 },
-            zoom: 14
+    $(document).ready(function () { 
+        Initialize();
+    });
+    function Initialize() {
+        google.maps.visualRefresh = true;
+        var start = new google.maps.LatLng(63.1766832, 14.636068099999989); // Startplats Östersund på karta
+        var mapOptions = {
+            zoom: 5,
+            center: start,
+            mapTypeId: google.maps.MapTypeId.G_NORMAL_MAP
         };
-        const mapDiv = document.getElementById('map');
-        return new google.maps.Map(mapDiv, mapOptions);
-    }
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    function addMarkers(map) {
-        //const locations = {
-        //    operaHouse: { lat: -33.8567844, lng: 151.213108 },
-        //    tarongaZoo: { lat: -33.8472767, lng: 151.2188164 },
-        //    manlyBeach: { lat: -33.8209738, lng: 151.2563253 },
-        //    hyderPark: { lat: -33.8690081, lng: 151.2052393 },
-        //    theRocks: { lat: -33.8587568, lng: 151.2058246 },
-        //    circularQuay: { lat: -33.858761, lng: 151.2055688 },
-        //    harbourBridge: { lat: -33.852228, lng: 151.2038374 },
-        //    kingsCross: { lat: -33.8737375, lng: 151.222569 },
-        //    botanicGardens: { lat: -33.864167, lng: 151.216387 },
-        //    museumOfSydney: { lat: -33.8636005, lng: 151.2092542 },
-        //    maritimeMuseum: { lat: -33.869395, lng: 151.198648 },
-        //    kingStreetWharf: { lat: -33.8665445, lng: 151.1989808 },
-        //    aquarium: { lat: -33.869627, lng: 151.202146 },
-        //    darlingHarbour: { lat: -33.87488, lng: 151.1987113 },
-        //    barangaroo: { lat: - 33.8605523, lng: 151.1972205 }
-        //}
-        var allLocations = getAllActivities();
+        //GPS-koordinater: https://www.gpskoordinater.com/
+        //Inladdning av markörer
+        var data = [
+            { "Id": 1, "PlaceName": "Forsaleden", "GeoLong": "62.8988458", "GeoLat": "14.967649100000017", "type": "Vandring" },
+            { "Id": 2, "PlaceName": "Bydalen", "GeoLong": "63.10122430000001", "GeoLat": "13.796461099999988", "type": "Skidor" },
+            { "Id": 3, "PlaceName": "Hannacksjön", "GeoLong": "63.19891579999999", "GeoLat": "15.2974289", "type": "Fiske" },
+            { "Id": 4, "PlaceName": "Åre", "GeoLong": "63.3990428", "GeoLat": "13.081505800000059", "type": "Skidor" },
+            { "Id": 5, "PlaceName": "Stora rengen", "GeoLong": "58.2589227", "GeoLat": "15.720074699999941", "type": "Fiske" }
+        ];
 
-        const locations = allLocations.Latitude + allLocations.Longitude;
-        const markers = [];
-        for (const location in locations) {
-            const markerOptions = {
-                map: map,
-                position: locations[location],              
-                icon: "/Images/Fishmarker.png"
+        $.each(data, function (i, item) {
+            var icon;
+            if (item.type == "Skidor") {
+                icon = "/Images/Skiermarker.png"
             }
-            const marker = new google.maps.Marker(markerOptions);
-            markers.push(marker);
-        }
-        return markers;
-    }
-
-    function clusterMarkers(map, markers) {
-        const clustererOptions = { imagePath: "/Images/Skiermarker.png" }
-        const markerCluster = new MarkerClusterer(map, markers, clustererOptions);
-    }
-
-    function addPanToMarker(map, markers) {
-        let circle;
-        markers.map(marker => {
-            marker.addListener('click', event => {
-                const location = { lat: event.latLng.lat(), lng: event.latLng.lng() };
-                map.panTo(location);
-                if (circle) {
-                    circle.setMap(null);
-                }
-                circle = drawCircle(map, location);
+            else if (item.type == "Fiske") {
+                icon = "/Images/Fishmarker.png"
+            }
+            else if (item.type == "Vandring") {
+                icon = "/Images/Hikermarker.png"
+            }
+            var marker = new google.maps.Marker({
+                'position': new google.maps.LatLng(item.GeoLong, item.GeoLat),
+                'map': map,
+                'animation': google.maps.Animation.DROP,
+                'title': item.PlaceName,
+                'Type': item.type,
+                'icon': icon
             });
-        });
-    }
+            var infowindow = new google.maps.InfoWindow({
+                content: "<div class='infoDiv'><h3>" + item.PlaceName + "<br />" + "<br />" + "<h5>" + item.type + "</div>"
+            });
 
-    function drawCircle(map, location) {
-        const circleOptions = {
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 1,
-            map: map,
-            center: location,
-            radius: 800
-        }
-        const circle = new google.maps.Circle(circleOptions);
-        return circle;
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+        })
     }
 
     // Hämtar första datan vid inladdning av sida.
